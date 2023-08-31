696e010b6c0e155dc84c4cebf1bb4b5ca5d413b5
rakesh <rakeshreddy@gmail.com>
2023-08-31 10:20:56 +0530

    modified names

--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 name1=rakesh
 name2=mouli
-echo "rakesh: welcome to hyd"
-echo "mouli: welcome to jagdalpur"
-echo "rakesh: how are you"
-echo "mouli: fine, how are you"
+echo "$name1: welcome to hyd $name2"
+echo "$name2: welcome to jagdalpur $name1"
+echo "$name1: how are you $name2"
+echo "$name2: fine, how are you $name1"
