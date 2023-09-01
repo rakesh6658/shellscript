277587f59ae6b28b6e45b393f2e3cb28d9d3a7f4
rakesh <rakeshreddy@gmail.com>
2023-09-01 20:55:26 +0530

    added deletion.sh

--- deletion.sh
+++ deletion.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-location=/d/work/shellscript
+location=/home/centos/shellscript
 date=$(date "+%Y-%m-%d-%I-%M-%S")
 echo "$date"
+scriptname=$0
+logfile=$scriptname-$date
+filenames=$(find $location -name "*.log" -type f -mtime -32)
+
+while read -r line
+do
+  echo "deleting $line " >> $logfile
+  rm -rf $line
+done >>> $filenames
+
 
