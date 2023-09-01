dde0e68d4f17c78e84cba30ec26f4b28636b3a93
rakesh <rakeshreddy@gmail.com>
2023-09-01 21:14:13 +0530

    modified

--- deletion.sh
+++ deletion.sh
@@ -5,7 +5,7 @@ date=$(date "+%Y-%m-%d-%I-%M-%S")
 #echo "$date"
 logfile="deleted-$date.log"
 filenames=$(find $location -name "*.log" -type f -mtime -32)
-
+echo "$filenames
 while read -r line
 do
   echo "deleting $line " >> $logfile
