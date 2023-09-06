77900614b4715ee7ca2f619246a1a2fec15c46c5
rakesh <rakeshreddy@gmail.com>
2023-09-06 18:43:15 +0530

    added createec2.sh

---
+++ createec2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+names=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+instance_type=""
+imageid=ami-03265a0778a880afb
+security_group=sg-03161170439f7f149
+domain_name=lakshman.tech
+for i in "${names[@]}"
+do
+echo "$i"
+if [[$i == mongodb || $i == mysql]]
+then
+instance_type="t3.medium"
+else
+instance_type="t2.micro"
+ipaddress=$(aws ec2 run-instances --image-id $imageid --instance-type $instance_type --security-group-ids $security_group  --tag-specifications "ResourceType=instance,Tags= [ {Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress'
+)
+echo "instance 
+done
+
