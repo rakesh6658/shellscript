0aa2b3ed133ca1ac484e09e5e506d92779076ef4
rakesh <rakeshreddy@gmail.com>
2024-05-25 21:58:42 +0530

    added practice.sh

--- practice.sh
+++ practice.sh
@@ -5,4 +5,18 @@ then
 echo " user is not root "
 exit 1
 fi
-yum install gitt -y
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+echo "installation is failure"
+else 
+echo "installation is success"
+fi
+yum install postfix -y
+if [ $? -ne 0 ]
+then
+echo "installation is failure"
+else 
+echo "installation is success"
+fi
