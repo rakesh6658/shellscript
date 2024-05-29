a1756e3ae743f35724e745974202920705ad72ca
rakesh <rakeshreddy@gmail.com>
2024-05-29 22:00:05 +0530

    updated deletelogs.sh

--- deletelogs.sh
+++ deletelogs.sh
@@ -2,11 +2,13 @@
 date=$(date +%F:%H:%M:%S)
 scriptname=$0
 location=/home/centos/shellscriptlogs
-logfile=$scriptname-$date.log
+logfile=$location/$scriptname-$date.log
+
 
 echo "$date $scriptname"
 filestodelete=$(find /home/centos/applogs -name "*.log" -type f -mtime +14)
 for i in $filestodelete
 do
 echo "$i" &>> $logfile
+rm -rf $i 
 done
