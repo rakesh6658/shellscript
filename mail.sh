9489a212d67ee50c43b3bbe901e2bdea1978c61c
rakesh <rakeshreddy@gmail.com>
2024-05-30 03:15:02 +0530

    updated deletelogs.sh

--- mail.sh
+++ mail.sh
@@ -4,6 +4,6 @@ body=$3
 alert_type=$4
 echo "all attributes : $@"
 message=$(sed -e "s|TEAM_NAME|$Team_name|" -e "s|ALERT_TYPE|$alert_type|" -e "s|MESSAGE|$body|" mail.html)
-mailx -s "$(echo -e "$message: text/html")" | mail -s "$alert_type" "$To_address"
+echo -e "$message" | mail -s "$alert_type \nContent-Type: text/html" "$To_address"
 
 #mailx -s "$(echo -e "Newsletter issue 3\nContent-Type: text/html")"
