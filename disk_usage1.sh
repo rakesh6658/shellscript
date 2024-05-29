db6ce0cfcff2f2ded8b22a60ba397aba41aa4679
rakesh <rakeshreddy@gmail.com>
2024-05-30 02:23:30 +0530

    updated deletelogs.sh

--- disk_usage1.sh
+++ disk_usage1.sh
@@ -6,11 +6,13 @@ logfile=$location/$script_name-$date
 threshold=1
 message=
 disk_usage=$(df -hT | grep -E -v 'tmpfs|Filesystem') 
-echo "$usage"
+
 while IFS= read line
 do
  usage = $(echo $line | awk '{print $6}' | cut -d "%" -f 1 )
 partition= $(echo $line | awk '{print $1}' )
 message+="high disk uasge on $partition : $usage \n"
+done <<< $disk_usage
 echo -e "$message"
+
 
