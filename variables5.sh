a35712bb1ac80a8d31f763eee375200ef1131751
rakesh <rakeshreddy@gmail.com>
2025-01-18 22:22:42 +0530

    added variables5.sh

---
+++ variables5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# prompting user to enter password
+echo "enter your username"
+read username
+echo "enter your password"
+read password
+echo "your username is $username and password is $password"
