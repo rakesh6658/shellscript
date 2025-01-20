714daceae1318a21c5ac3b45a8dbbdf81cedbfcb
rakesh <rakeshreddy@gmail.com>
2025-01-20 13:44:08 +0530

    created mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -8,3 +8,19 @@ echo " not a root user "
 exit 14
 fi
 yum install mysql -y
+exit=$?
+if [ $exit == 0 ]
+then
+echo "installation is success "
+else 
+echo " installation is failure"
+fi
+yum install gitt -y
+exit=$?
+if [ $exit == 0 ]
+then
+echo "installation is success "
+else 
+echo " installation is failure"
+fi
+
