65fd5b84e5add6a15e6a1a6239a618724a8c119b
rakesh <rakeshreddy@gmail.com>
2025-01-21 12:05:58 +0530

    modified forloop.sh

--- forloop.sh
+++ forloop.sh
@@ -5,7 +5,29 @@ if [ $user -ne 0 ]
 then
 echo "user is not root user"
 fi
+LOGDIR=/home/centos/shellscriptlogs
+name=$0
+date=$(date +%F-%H-%M-%S)
+logfile=$LOGDIR/$name-$date
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+validate(){
+if [ $1 == 0 ]
+then
+echo -e " $G $2 installation .... success $N"
+else
+echo -e " $R $2 installation ... failure $N"
+fi     
+}
 for i in $@
 do
-yum install $i -y
+$i --version
+if [ $? == 0 ]
+then
+echo -e " $Y $i is already installed $N "
+else
+yum install $i -y &>> $logfile
+validate $? $i 
 done
