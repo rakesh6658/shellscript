496964ab2dcdfd13afc5c42799fcf1cdaafff0ad
rakesh <rakeshreddy@gmail.com>
2025-01-23 14:36:09 +0530

    created deletelogs.sh

--- deletelog.sh
+++ deletelog.sh
@@ -5,8 +5,8 @@ date=$(date +%F-%H-%M-%S)
 name=$0
 logfile=$deletelogsdir/$name-$date
 files_to_delete=$(find $logsdir -name "*.log" -type f -mtime +370)
-echo "files to delete $files_to_delete"
-while read  line;
+ #echo "files to delete $files_to_delete"
+while read -r line;
 do
     echo $line
 done < $files_to_delete
